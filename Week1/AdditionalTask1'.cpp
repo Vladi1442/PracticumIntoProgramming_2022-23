6f920c4bf07c1ce08ca25b12580c4e86b6ec0323
Vladimir <fibonaci.233@gmail.com>
2022-10-24 22:46:54 +0300

    Update to AdditionalTask1

---
+++ Week1/AdditionalTask1'.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    // Напишете програма на C++, която извежда следния текст на екрана:
+    // Oh what
+    // a happy day!
+    // Oh yes,
+    // what a happy day!
+
+    std::cout << "Oh what" << std::endl;
+    std::cout << "a happy day!" << std::endl;
+    std::cout << "Oh yes," << std::endl;
+    std::cout << "what a happy day!" << std::endl;
+
+	return 0;
+}
