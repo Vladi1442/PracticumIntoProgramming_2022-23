ee92cf0ddf52d62b37a27ebe6c420054061a807a
Vladimir <95411701+Vladi1442@users.noreply.github.com>
2022-10-24 22:48:38 +0300

    Update AdditionalTask1.cpp

--- Week1/AdditionalTask1.cpp
+++ Week1/AdditionalTask1.cpp
@@ -2,7 +2,16 @@
 
 int main() {
 
-
-
+	// Напишете програма на C++, която извежда следния текст на екрана:
+    	// Oh what
+    	// a happy day!
+    	// Oh yes,
+	// what a happy day!
+ 
+        std::cout << "Oh what" << std::endl;
+        std::cout << "a happy day!" << std::endl;
+        std::cout << "Oh yes," << std::endl;
+        std::cout << "what a happy day!" << std::endl;
+	
 	return 0;
 }
